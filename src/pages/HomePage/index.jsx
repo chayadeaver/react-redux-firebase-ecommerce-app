f1f3753239e46ab2f4e4cb83a88c8a7804f20462
chayadeaver <chayadeaver@gmail.com>
2021-04-02 15:44:25 -0400

    changed file type

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from '../../components/Directory/index';
+
+const HomePage = props => {
+  return (
+    <section>
+      <Directory />
+    </section>
+  );
+};
+
+export default HomePage;
