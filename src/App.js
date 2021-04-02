e36e1a3f16f5e3150553e55ff9ab05c8ee8f0716
chayadeaver <chayadeaver@gmail.com>
2021-04-02 15:23:37 -0400

    simple h2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React from 'react';
+import './default.scss';
+import Header from './components/Header/index';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <h2>
+        Hello World!
+      </h2>
     </div>
   );
 }
