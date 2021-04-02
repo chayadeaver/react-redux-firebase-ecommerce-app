5c8c2f14bd60631ed5051d8da1be279b0410131d
chayadeaver <chayadeaver@gmail.com>
2021-04-02 15:43:29 -0400

    added HomePage component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './default.scss';
-import Header from './components/Header/index';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>
-        Hello World!
-      </h2>
+      <HomePage />
     </div>
   );
 }
