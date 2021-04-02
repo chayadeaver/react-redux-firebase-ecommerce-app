adf69522f2a9afb6051ea0e6f999c40e3fcb5d52
chayadeaver <chayadeaver@gmail.com>
2021-04-02 17:07:11 -0400

    created registration page

---
+++ src/pages/Registration/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Registration extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Registration page</h1>
+      </div>
+    )
+  }
+}
+
