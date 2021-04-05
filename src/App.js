48290fc4b869aa136f254d0f81fc09212632bd34
chayadeaver <chayadeaver@gmail.com>
2021-04-05 14:43:39 -0400

    import layouts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+
+
+// layouts
+import MainLayout from './layouts/MainLayout';
+import HomePageLayout from './layouts/HomePageLayout';
+
+//pages
 import './default.scss';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Registration from './pages/Registration';
 
+
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/registration" component={Registration} />
+      <Switch>
+          <Route exact path="/" render={() => (
+            <HomePageLayout>
+              <HomePage />
+            </HomePageLayout>
+          )}/>
+          <Route path="/registration" render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )} />
         </Switch>
-      </div>
     </div>
   );
 }
