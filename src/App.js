0a90dc0d2e852d4744f613ce7a13985900e0f821
chayadeaver <chayadeaver@gmail.com>
2021-04-06 22:38:30 -0400

    installed firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
+import { auth } from './firebase/utils'
 
 
 // layouts
@@ -11,26 +12,52 @@ import './default.scss';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Registration from './pages/Registration';
+import Login from './pages/Login/index';
+
+const initialState = {
+  currentUser = null
+}
 
 
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ...initialState
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-          <Route exact path="/" render={() => (
-            <HomePageLayout>
-              <HomePage />
-            </HomePageLayout>
-          )}/>
-          <Route path="/registration" render={() => (
-            <MainLayout>
-              <Registration />
-            </MainLayout>
-          )} />
-        </Switch>
-    </div>
-  );
+  componentDidMount(){
+
+  }
+
+  componentWillUnmount(){
+    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Switch>
+            <Route exact path="/" render={() => (
+              <HomePageLayout>
+                <HomePage />
+              </HomePageLayout>
+            )}/>
+            <Route path="/registration" render={() => (
+              <MainLayout>
+                <Registration />
+              </MainLayout>
+            )} />
+            <Route path="/login" render={() => (
+              <MainLayout>
+                <Login />
+              </MainLayout>
+            )} />
+          </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
