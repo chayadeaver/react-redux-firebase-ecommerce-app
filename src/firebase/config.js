58168dff1eadf568cceeeeecce9891c737978a32
chayadeaver <chayadeaver@gmail.com>
2021-04-06 22:37:25 -0400

    firebase config setup

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig =  {
+      apiKey: "AIzaSyC5qr2D0SaLXU0qhWqf5P06TUqWsaZXwMY",
+      authDomain: "ecommerce-site-8dc24.firebaseapp.com",
+      projectId: "ecommerce-site-8dc24",
+      storageBucket: "ecommerce-site-8dc24.appspot.com",
+      messagingSenderId: "41231587332",
+      appId: "1:41231587332:web:54f15e080b8ad00a487d51"
+    };
