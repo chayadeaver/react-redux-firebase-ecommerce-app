53b0ba0efd0c4a612cf5f34cef9fe38dbd862295
chayadeaver <chayadeaver@gmail.com>
2021-04-08 11:21:50 -0400

    added signup component to registration page

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
+import Signup from '../../components/Signup/index';
 
 export default class Registration extends Component {
   render() {
-    return (
-      <div>
-        <h1>Registration page</h1>
-      </div>
-    )
+    return <Signup />
   }
 }
 
