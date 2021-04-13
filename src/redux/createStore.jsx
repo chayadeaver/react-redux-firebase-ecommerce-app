719ef8a13718d2af8fcf38328f8a233e7672a06b
chayadeaver <chayadeaver@gmail.com>
2021-04-13 16:05:51 -0400

    initialize redux sage

--- src/redux/createStore.jsx
+++ src/redux/createStore.jsx
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddle from 'redux-saga';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
 
-export const middlewares = [thunk, logger];
+const sagaMiddleware = createSagaMiddle();
+export const middlewares = [thunk, sagaMiddleware, logger];
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)));
-
+sagaMiddleware.run(rootSaga);
 
 export default store;
