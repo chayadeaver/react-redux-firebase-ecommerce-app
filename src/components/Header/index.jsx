2898f05fdc485b51d9a282da3e6961f9f73f48c9
chayadeaver <chayadeaver@gmail.com>
2021-04-19 16:54:02 -0400

    added nav element

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -25,6 +25,22 @@ const Header = props => {
           <img src={Logo} alt="SimpleTut LOGO"/>
           </Link>
         </div>
+
+        <nav>
+          <ul>
+          <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/search">
+                Search
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
         <div className="callToActions">
 
           {currentUser && (
