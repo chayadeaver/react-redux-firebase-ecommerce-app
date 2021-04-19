ba4bffaf6675afa7945c4895819895612d401141
chayadeaver <chayadeaver@gmail.com>
2021-04-18 21:52:37 -0400

    rendering the new products onto page

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { addProductStart, fetchProductsStart, deleteProductStart } from './../../redux/Products/products.actions'
 import Modal from './../../components/Modal';
 import FormInput from './../../components/forms/FormInput';
 import FormSelect from './../../components/forms/FormSelect';
 import Button from './../../components/forms/Button';
 import './styles.scss';
 
+const mapState = ({ productsData }) => ({
+  products: productsData.products
+});
+
 
 const Admin = props => {
+  const { products } = useSelector(mapState);
   const dispatch = useDispatch();
   const [hideModal, setHideModal] = useState(true);
   const [productCategory, setProductCategory] = useState('mens');
   const [productName, setProductName] = useState('');
   const [productThumbnail, setProductThumbnail] = useState('');
   const [productPrice, setProductPrice] = useState(0);
+
+  useEffect(() => {
+    dispatch(
+      fetchProductsStart()
+    );
+
+  }, []);
 
   const toggleModal = () => setHideModal(!hideModal);
 
@@ -22,8 +35,25 @@ const Admin = props => {
     toggleModal
   };
 
+  const resetForm = () => {
+    setHideModal(true);
+    setProductCategory('mens');
+    setProductName('');
+    setProductThumbnail('');
+    setProductPrice(0);
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
+
+    dispatch(
+      addProductStart({
+        productCategory,
+        productName,
+        productThumbnail,
+        productPrice
+    }));
+    resetForm();
   };
 
 
@@ -90,6 +120,54 @@ const Admin = props => {
           </form>
         </div>
       </Modal>
+
+      <div className="manageProducts">
+        <table border="0" cellPadding="0" cellSpacing="0">
+          <tbody>
+            <tr>
+              <th>
+                <h1>
+                  Manage Products
+                </h1>
+              </th>
+            </tr>
+            <tr>
+              <td>
+                <table className="results" border="0" cellPadding="10" cellSpacing="0">
+                  <tbody>
+                    {products.map((product, index) => {
+                      const {
+                        productName,
+                        productThumbnail,
+                        productPrice,
+                        documentID
+                      } = product;
+                      return (
+                        <tr>
+                          <td>
+                            <img className="thumb" src={productThumbnail} alt=""/>
+                          </td>
+                          <td>
+                            {productName}
+                          </td>
+                          <td>
+                            ${productPrice}
+                          </td>
+                          <td>
+                            <Button onClick={() => dispatch(deleteProductStart(documentID))}>
+                              Delete
+                            </Button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
 
     </div>
