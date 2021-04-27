17a1ba8c155504360fdb7819765ce257ddb86786
chayadeaver <chayadeaver@gmail.com>
2021-04-27 16:18:57 -0400

    create cart component to contain checkout

---
+++ src/pages/Cart/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Checkout from '../../components/Checkout/index';
+
+const Cart = (props) => {
+  return (
+    <div>
+      <Checkout />
+    </div>
+  );
+}
+
+export default Cart;
