f643d809864021ccac7a78719b43e857fa59d717
chayadeaver <chayadeaver@gmail.com>
2021-04-27 22:03:24 -0400

    import persistStore and exported persistor

--- src/redux/createStore.jsx
+++ src/redux/createStore.jsx
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddle from 'redux-saga';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist'
 
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
@@ -14,4 +15,9 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)));
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {
+  store,
+  persistor
+};
