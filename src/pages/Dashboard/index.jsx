66b8dc226347fea134fa4b5b98bc41b71bd83021
chayadeaver <chayadeaver@gmail.com>
2021-05-26 15:03:48 -0400

    renders a user's order history

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector} from 'react-redux'
+import { getUserOrderHistory } from './../../redux/Orders/orders.actions';
 import './styles.scss'
+import OrderHistory from '../../components/OrderHistory/index';
+
+const mapState = ({ user, ordersData }) => ({
+  currentUser: user.currentUser,
+  orderHistory: ordersData.orderHistory.data
+});
 
 const Dashboard = props => {
+  const dispatch = useDispatch();
+  const { currentUser, orderHistory } = useSelector(mapState);
+
+  useEffect(() => {
+    dispatch(
+      getUserOrderHistory(currentUser.id)
+      );
+  }, []);
+
+
   return (
-    <h1>
-      You're logged in!
-    </h1>
+    <div>
+      <h1>
+        Order History
+      </h1>
+      <OrderHistory orders={orderHistory}/>
+    </div>
   )
 }
 
